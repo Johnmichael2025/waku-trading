14fbc0699702814914390aadafad451562b0aa1b
joven.pancho@essilor.com <joven.pancho@essilor.com>
2025-02-06 21:50:05 +0800

    fix build error

--- src/app/admin-portal/layout.tsx
+++ src/app/admin-portal/layout.tsx
@@ -1,5 +1,6 @@
 import SidebarNavigation from "@/components/SidebarNavigation";
 import { ADMIN_PORTAL_NAVIGATION } from "@/constants/admin-portal-navigation.constant";
+import { Suspense } from "react";
 
 export default function AdminPortalLayout({
   children,
@@ -8,8 +9,10 @@ export default function AdminPortalLayout({
 }>) {
   return (
     <>
-      <SidebarNavigation navLinks={ADMIN_PORTAL_NAVIGATION} />
-      <div className="md:ml-[270px] p-10">{children}</div>
+      <Suspense>
+        <SidebarNavigation navLinks={ADMIN_PORTAL_NAVIGATION} />
+        <div className="md:ml-[270px] p-10">{children}</div>
+      </Suspense>
     </>
   );
 }
