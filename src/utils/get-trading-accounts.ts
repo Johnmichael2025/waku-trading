646090fe5bb35c7f024d38c6c47397dab2633226
joven.pancho@essilor.com <joven.pancho@essilor.com>
2025-02-06 23:03:40 +0800

    add debugging log

--- src/utils/get-trading-accounts.ts
+++ src/utils/get-trading-accounts.ts
@@ -1,4 +1,5 @@
 export const getTradingAccounts = async () => {
+  console.log(process.env.NEXT_PUBLIC_API_BASE_URL, 'base url')
   try {
     const res = await fetch(
       `${process.env.NEXT_PUBLIC_API_BASE_URL}/trading-accounts`
