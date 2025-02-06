b5fecb7d9b6ddf384e84bb6e726863d1369fab31
joven.pancho@essilor.com <joven.pancho@essilor.com>
2025-02-06 21:21:45 +0800

    fix build errors

--- src/app/api/trading-accounts/route.ts
+++ src/app/api/trading-accounts/route.ts
@@ -1,7 +1,7 @@
 import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
-export async function GET(request: Request) {
+export async function GET() {
   try {
     const accounts = await prisma.tradingAccount.findMany();
     return NextResponse.json(accounts);
